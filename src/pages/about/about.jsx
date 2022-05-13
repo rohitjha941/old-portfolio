523e424efaead4c8d0d38ccc58a88a9ece6162f5
Rohit Jha <rjha@ph.iitr.ac.in>
2022-05-14 02:42:53 +0530

    chore: removed unused imports

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import styles from "./about.module.scss";
 import Heading from "../../components/heading/heading";
 
--- src/pages/projects/project.jsx
+++ src/pages/projects/project.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import "./projects.scss";
 import Rethink from "../../static/projects/rethink.png";
 import Esummit from "../../static/projects/esummit.png";
